ffcb22634bef80ced60794d50527532b3a711c47
Inkom007 <inkom0703@gmail.com>
2024-09-08 15:27:03 +0100

    done

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const NewContact = await createFakeContact();
+  contacts.push(NewContact);
+  await writeContacts(contacts);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,16 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContact = [];
+  for (let i = 0; i < number; i++) {
+    const contact = await createFakeContact();
+    newContact.push(contact);
+  }
+  const addContact = contacts.concat(newContact);
+  writeContacts(addContact);
+};
 
 generateContacts(5);
